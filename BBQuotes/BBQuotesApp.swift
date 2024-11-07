2e9af196ee79338d4315e5c782be5021db7e39bf
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-11-07 22:37:40 +0930

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Jinyuan Zhang on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
